7ccf936f65d090e916574c105d64536024051fd2
sufyanazeem <sufyanazeem93@gmail.com>
2024-07-11 13:13:48 +0500

    Basic Structure Set

---
+++ components/Sidebar/Sidebar.tsx
@@ -0,0 +1,16 @@
+"use client"
+import React from 'react'
+import styled from 'styled-components';
+
+
+const Sidebar = () => {
+  return (
+    <SidebarStyled>
+      Sidebar
+    </SidebarStyled>
+  )
+}
+
+const SidebarStyled = styled.nav``;
+
+export default Sidebar
