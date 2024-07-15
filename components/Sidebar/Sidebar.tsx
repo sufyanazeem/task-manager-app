4281157e3bb2a7ab9fb9baafdcad035396d3e693
sufyanazeem <sufyanazeem93@gmail.com>
2024-07-15 22:31:33 +0500

    Sidebar Icons

--- components/Sidebar/Sidebar.tsx
+++ components/Sidebar/Sidebar.tsx
@@ -1,25 +1,61 @@
-"use client"
-import { useGlobalState } from '@/context/globalProvider'
-import React from 'react'
-import styled from 'styled-components'
-
+"use client";
+import { useGlobalState } from "@/context/globalProvider";
+import Image from "next/image";
+import React from "react";
+import styled from "styled-components";
+import menu from "./../../utils/menu";
+import Link from "next/link";
+import { usePathname, useRouter } from "next/navigation";
 
 const Sidebar = () => {
+  const router = useRouter();
 
-  const {theme} = useGlobalState();
-  console.log(theme)
+  const pathname = usePathname();
+
+  const { theme } = useGlobalState();
+  const handleClick = (link: string) => {
+    router.push(link);
+  };
+
   return (
     <SidebarStyled theme={theme}>
-      Sidebar
+      <div className="profile">
+        <div className="profile-overlay"></div>
+        <div className="image">
+          <Image src="/avatar1.png" alt="profile-pic" width={70} height={70} />
+        </div>
+        <h1>
+          <span>JOD</span>
+          <span>BOT</span>
+        </h1>
+        <ul className="nav-items">
+        {menu.map((item) => {
+          const link = item.link;
+          return (
+            <li
+              key={item.id}
+              className={`nav-item ${pathname === link ? "active" : ""}`}
+              onClick={() => {
+                handleClick(link);
+              }}
+            >
+              {item.icon}
+              <Link href={link}>{item.title}</Link>
+            </li>
+          );
+        })}
+      </ul>
+      </div>
     </SidebarStyled>
-  )
-}
+  );
+};
 
-const SidebarStyled = styled.nav`  
-position:relative;
-width: ${(props) => props.theme.sidebarWidth};
+const SidebarStyled = styled.nav`
+  position: relative;
+  width: ${(props) => props.theme.sidebarWidth};
   background-color: ${(props) => props.theme.colorBg2};
   border: 2px solid ${(props) => props.theme.borderColor2};
-  border-radius: 1rem;`;
+  border-radius: 1rem;
+`;
 
 export default Sidebar;
