42254bf70be2f315b4035cf7eda74a321cf97b84
sufyanazeem <sufyanazeem93@gmail.com>
2024-07-15 17:12:57 +0500

    Sidebar Border

--- components/Sidebar/Sidebar.tsx
+++ components/Sidebar/Sidebar.tsx
@@ -1,16 +1,25 @@
 "use client"
+import { useGlobalState } from '@/context/globalProvider'
 import React from 'react'
-import styled from 'styled-components';
+import styled from 'styled-components'
 
 
 const Sidebar = () => {
+
+  const {theme} = useGlobalState();
+  console.log(theme)
   return (
-    <SidebarStyled>
+    <SidebarStyled theme={theme}>
       Sidebar
     </SidebarStyled>
   )
 }
 
-const SidebarStyled = styled.nav``;
+const SidebarStyled = styled.nav`  
+position:relative;
+width: ${(props) => props.theme.sidebarWidth};
+  background-color: ${(props) => props.theme.colorBg2};
+  border: 2px solid ${(props) => props.theme.borderColor2};
+  border-radius: 1rem;`;
 
-export default Sidebar
+export default Sidebar;
--- providers/ContextProvider.tsx
+++ providers/ContextProvider.tsx
@@ -1,13 +1,12 @@
 "use client";
 import React from "react";
 import { GlobalProvider } from "../context/globalProvider";
-import { Toaster } from "react-hot-toast";
-
 interface Props {
   children: React.ReactNode;
 }
 
 function ContextProvider({ children }: Props) {
+
   const [isReady, setIsReady] = React.useState(false);
 
   React.useEffect(() => {
@@ -15,18 +14,12 @@ function ContextProvider({ children }: Props) {
       setIsReady(true);
     }, 250);
   }, []);
-
   if (!isReady) {
-    return (
-      <div className="w-full h-full flex items-center justify-center">
-        <span className="loader"></span>
-      </div>
-    );
-  }
 
+    return null;
+  }
   return (
     <GlobalProvider>
-      <Toaster />
       {children}
     </GlobalProvider>
   );
